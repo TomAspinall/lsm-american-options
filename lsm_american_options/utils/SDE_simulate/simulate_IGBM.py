791b7f900a42cf096df15ac79133bc7632a9a7f4
TomAspinall <tomaspinall2512@gmail.com>
2024-04-16 19:35:08 +1000

    update simulate_IGBM.py

--- lsm_american_options/utils/SDE_simulate/simulate_IGBM.py
+++ lsm_american_options/utils/SDE_simulate/simulate_IGBM.py
@@ -11,9 +11,9 @@ def simulate_IGBM(
         S0: Number, 
         dt: Number
         ) -> np.ndarray:
-    """
-    simulate random paths of the inomogeneous geometric Brownian motion stochastic differential process through Monte Carlo simulation.
-    """
+
+    ## Constant:
+    adjusted_risk = 0.5 * (sigma ** 2)
 
     ## Dimension 1:
     number_steps = t / dt
@@ -24,20 +24,24 @@ def simulate_IGBM(
         number_simulations = n
     else:
         number_simulations = n + 1
+
+    ## Even number of simulations conducted regardless:
     number_loops = int(number_simulations / 2)
 
+    ## Thetic and antithetic columns:
+    simulated_value_columns = np.arange(0,number_simulations,2)
+    antithetic_value_columns = simulated_value_columns + 1
 
     ## shock:
     shock = (np.random.normal(scale=sigma * sqrt(dt), size = number_loops * number_steps_total)).reshape((number_steps_total, number_loops))
 
+    ## Output array:
     output = np.ndarray((number_steps_total, number_simulations))
 
     ## Initial values;
     output[0] = log(S0)
 
-    adjusted_risk = 0.5 * (sigma ** 2)
-    simulated_value_columns = np.arange(0,number_simulations,2)
-    antithetic_value_columns = simulated_value_columns + 1
+    ## Begin Monte Carlo simulation:
     for t in range(number_steps_total - 1):
 
         ## Log-distribution:
