b686734d61786c3cee2ca07bd0b01a7821fbe84d
TomAspinall <tomaspinall2512@gmail.com>
2024-06-06 19:53:58 +1000

    hotfix orthogonal PDF

--- option_pricing/orthogonal_polynomials/PDF/_PDF.py
+++ option_pricing/orthogonal_polynomials/PDF/_PDF.py
@@ -17,6 +17,7 @@ Possible orthogonal PDF's:
     JACOBI
 """
 
+# Laguerre:
 
 def laguerre(n, m, x):
     c_x = ((-1) ** m) * _comb(n, n-m) * (1/_factorial(m))
@@ -27,14 +28,14 @@ def laguerre(n, m, x):
 
 def legendre(n, m, x):
 
-    c_x = (-1 ** m) * _comb(n, m) * _comb(2*n - 2*m, n)
-    g_m_x = x ** (n - 2 * m)
+    c_x = ((-1) ** m) * _comb(n, m) * _comb(2*n - 2*m, n)
+    g_m_x = x ** (n - (2 * m))
     return c_x * g_m_x
 
 # Chebyshev:
 
 def chebyshev(n, m, x):
-    c_x = (-1 ** m) * _factorial(n - m - 1) / \
+    c_x = ((-1) ** m) * _factorial(n - m - 1) / \
         (_factorial(m) * _factorial(n - 2 * m))
     g_m_x = (2 * x) ** (n - (2 * m))
     return c_x * g_m_x
@@ -42,7 +43,7 @@ def chebyshev(n, m, x):
 # Hermite:
 
 def hermite(n, m, x):
-    c_x = (1 ** m) / (_factorial(m) * (2 ** m) * (_factorial(n - 2 * m)))
+    c_x = ((-1) ** m) / (_factorial(m) * (2 ** m) * (_factorial(n - 2 * m)))
     g_m_x = x ** (n - 2 * m)
     return c_x * g_m_x
 
@@ -52,4 +53,3 @@ def jacobi(n, m, x, alpha, beta):
     c_x = _comb(n + alpha, m) * _comb(n + beta, n - m)
     g_m_x = ((x - 1) ** (n - m)) * ((x + 1) ** m)
     return c_x * g_m_x
-
