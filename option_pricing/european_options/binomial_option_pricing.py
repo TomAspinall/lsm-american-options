b36309cc3771ff63c37f3628e8be95b61f2b6ac6
TomAspinall <tomaspinall2512@gmail.com>
2024-10-09 21:34:45 +1000

    apply blacks formatting to European options directory

--- option_pricing/european_options/binomial_option_pricing.py
+++ option_pricing/european_options/binomial_option_pricing.py
@@ -1,10 +1,12 @@
-from math import exp, sqrt, ceil
+from math import ceil, exp, sqrt
+
 import numpy as np
+
+# Discount Factor:
+from ..utils.discount import discount
 
 # __name__ = 'option_pricing.european_options.binomial_option_pricing'
 
-## Discount Factor:
-from .._utils.discount import discount
 
 def binomial_option_pricing_model(
     risk_free_rate,
@@ -13,40 +15,44 @@ def binomial_option_pricing_model(
     stock_price,
     strike_price,
     n,
-    call_option = True
+    call_option=True
 ):
 
-    ## Discount / premium rate between discrete steps:
+    # Discount / premium rate between discrete steps:
     discount_rate = discount(risk_free_rate, time_step)
     premium_rate = discount(-risk_free_rate, time_step)
 
-    ## Number of Discrete time steps:
+    # Number of Discrete time steps:
     number_periods = n / time_step
-    ## Total Periods evaluated (int):
+    # Total Periods evaluated (int):
     total_periods = ceil(number_periods)
 
-    ## Risk Neutral Probabilites:
-    prob_up   = exp(sigma * sqrt(time_step))
-    prob_down =  exp(-sigma * sqrt(time_step))
-    
-    ## Length of output:
+    # Risk Neutral Probabilites:
+    prob_up = exp(sigma * sqrt(time_step))
+    prob_down = exp(-sigma * sqrt(time_step))
+
+    # Length of output:
     cols = np.arange(start=1, stop=total_periods+1)
 
-    ## Prices at option maturity:
-    terminal_stock_prices = stock_price * prob_down ** (total_periods - cols) * prob_up ** (cols - 1)
+    # Prices at option maturity:
+    terminal_stock_prices = stock_price * \
+        prob_down ** (total_periods - cols) * prob_up ** (cols - 1)
 
-    ## Payoff calculation:
+    # Payoff calculation:
     if call_option:
         option_tree = np.maximum(terminal_stock_prices - strike_price, 0)
     else:
         option_tree = np.maximum(strike_price - terminal_stock_prices, 0)
 
-    ## Discount likelihood according to probabilities:
-    probability = (premium_rate-prob_down)/(prob_up-prob_down) if prob_up-prob_down != 0 else 1
+    # Discount likelihood according to probabilities:
+    probability = (premium_rate-prob_down) / \
+        (prob_up-prob_down) if prob_up-prob_down != 0 else 1
     q_probability = 1 - probability
 
-    ## Recursive option pricing accoriding to risk-neutral probabilities of (up) vs (down):
+    # Recursive option pricing accoriding to risk-neutral probabilities of (up) vs (down):
     i = 0
-    for i in range(total_periods -1 , 0, -1):
-        option_tree[:i] = discount_rate * (probability * option_tree[:i] + q_probability * option_tree[1:(i+1)])
+    for i in range(total_periods - 1, 0, -1):
+        option_tree[:i] = discount_rate * \
+            (probability * option_tree[:i] +
+             q_probability * option_tree[1:(i+1)])
     return option_tree[i]
--- option_pricing/european_options/monte_carlo_simulation.py
+++ option_pricing/european_options/monte_carlo_simulation.py
@@ -1,33 +1,35 @@
-## Imports:
-from numbers import Number
-import numpy
-from typing import Optional
-
 # __name__ = 'option_pricing.european_options.monte_carlo_simulation'
 
-## Local Imports:
-from .._utils.discount import discount
+# Imports:
+from numbers import Number
+from typing import Optional
+
+import numpy
+
+# Local Imports:
+from ..utils.discount import discount
+
 
 def monte_carlo_simulation(payoff: numpy.array,
-                        strike_price: Number,
-                        n: Number,
-                        time_step: Number,
-                        risk_free_rate: Number,
-                        call_option: Optional[bool] = False,
-                        ):
+                           strike_price: Number,
+                           n: Number,
+                           time_step: Number,
+                           risk_free_rate: Number,
+                           call_option: Optional[bool] = False,
+                           ):
 
     # Nominal interest rate:
     nominal_interest_rate = risk_free_rate * time_step
 
-    ## Payoff calculation - option value at maturity:
+    # Payoff calculation - option value at maturity:
     if call_option:
         profit = numpy.maximum(payoff - strike_price, 0)
     else:
         profit = numpy.maximum(strike_price - payoff, 0)
 
-    ## A European Option never holds a "continuation value" (expected value of waiting to exercise):
+    # A European Option never holds a "continuation value" (expected value of waiting to exercise):
     # Discount payoffs:
     present_value = profit * discount(nominal_interest_rate, n)
 
-    ## Average simulated profit paths:
+    # Average simulated profit paths:
     return numpy.mean(present_value)
