8892245d79e3430d43b272e1a847025269e29b88
TomAspinall <tomaspinall2512@gmail.com>
2024-10-09 21:35:32 +1000

    apply Blacks formatting to real_options directory

--- option_pricing/real_options/monte_carlo_simulation.py
+++ option_pricing/real_options/monte_carlo_simulation.py
@@ -1,22 +1,18 @@
 
 
-import numpy as np
 from numbers import Number
-from typing import Optional, Union
+from typing import Union
+
+import numpy as np
 
 __name__ = 'option_pricing.real_options.monte_carlo_simulation'
 
 # Requirements:
-from .._utils.continuation_value import estimate_continuation_value
-from .._utils.discount import (
-        discount,
-        discount_array
-        )
-from .._utils.option_results import RealOption
+from ..utils.continuation_value import estimate_continuation_value
+from ..utils.discount import discount, discount_array
+from ..utils.option_results import RealOption
 
-from ..stochastic_differential_equations.geometric_brownian_motion import geometric_brownian_motion as GBM
-
-## Step 1 - Simulate asset prices:
+# Step 1 - Simulate asset prices:
 # state_variables = GBM(
 #     n = 100,
 #     t = 10,
@@ -37,6 +33,7 @@ from ..stochastic_differential_equations.geometric_brownian_motion import geomet
 # degree = 2
 # cross_product = True
 
+
 def monte_carlo_simulation(
         state_variables: np.ndarray,
         net_cash_flow: np.ndarray,
@@ -47,13 +44,13 @@ def monte_carlo_simulation(
         orthogonal: str = "Power",
         degree: int = 2,
         cross_product: bool = True,
-        ):
+):
 
-    ## State variables must be coerced as a 3-d array:
+    # State variables must be coerced as a 3-d array:
     if state_variables.ndim < 3:
         state_variables = state_variables.reshape(state_variables.shape + (1,))
 
-    ## Const:
+    # Const:
 
     # length rolumns: # simulations
     # length rows:    # discrete time periods
@@ -68,41 +65,47 @@ def monte_carlo_simulation(
 
     # Time period of backwards induction:
     termination_period = number_periods - 1
-    
+
     # Assertions:
     # assert type(K) is Number and not len(K) == number_simulations, "length of object 'K' does not equal 1 or number of columns of 'state_variables'!"
-    assert isinstance(construction_periods, int), "'construction_periods' must be of type 'int'"
-    assert not np.isnan(state_variables).any(), "NA's cannot be specified within 'state_variables'"
-    assert type(capital_expenditure) is not np.array or len(capital_expenditure) == len(net_cash_flow), "len(capital_expenditure) != len(net_cash_flow)"
-    assert number_periods == net_cash_flow.shape[0] and number_simulations == net_cash_flow.shape[1], "The first 2 dimensions of 'state_variables' must match the dimensions of 'net_cash_flow'"
+    assert isinstance(construction_periods,
+                      int), "'construction_periods' must be of type 'int'"
+    assert not np.isnan(state_variables).any(
+    ), "NA's cannot be specified within 'state_variables'"
+    assert type(capital_expenditure) is not np.array or len(capital_expenditure) == len(
+        net_cash_flow), "len(capital_expenditure) != len(net_cash_flow)"
+    assert number_periods == net_cash_flow.shape[0] and number_simulations == net_cash_flow.shape[
+        1], "The first 2 dimensions of 'state_variables' must match the dimensions of 'net_cash_flow'"
 
     ##############################################################################
     ################ Calculate Running Present Value (High Bias): ################
     ##############################################################################
 
-    ## Array of subsequent discounting of cash flows:
-    discount_arr = discount_array(nominal_interest_rate, np.array(range(number_periods)))
-    
-    ## Develop discount matrix:
+    # Array of subsequent discounting of cash flows:
+    discount_arr = discount_array(
+        nominal_interest_rate, np.array(range(number_periods)))
+
+    # Develop discount matrix:
     discount_matrix = np.eye(number_periods)
     diagonal = np.arange(0, number_periods)
     for i in diagonal[1:]:
         discount_matrix[diagonal[:-i] + i, diagonal[:-i]] = discount_arr[i]
     # Running Present Value (RPV) is the PV of all future net cash flows:
     running_present_value = (net_cash_flow.T @ discount_matrix).T
-    ## TODO: RPV at terminal simulated price = 0? Assumption of if operating in this period or not?
+    # TODO: RPV at terminal simulated price = 0? Assumption of if operating in this period or not?
 
     # Further discount the Running Present Value based upon waiting for construction to complete:
     if construction_periods > 0:
         running_present_value *= discount_arr[construction_periods]
-    
+
     # Immediate profit (high bias) is the net present value (NVP) conditional on:
         # Expending Initial Capital Expenditure
         # Waiting the construction period
         # obtaining the RPV at the time point the project becomes operational.
     profit = np.zeros(shape=(number_periods, number_simulations))
     # Offset the running present value actually attained within the immediate profit by the number of periods to wait for construction to complete:
-    profit[:(number_periods - construction_periods)] += running_present_value[construction_periods:number_periods]
+    profit[:(number_periods - construction_periods)
+           ] += running_present_value[construction_periods:number_periods]
     # Subtract the capital expenditure (which may be time varying):
     profit -= capital_expenditure
 
@@ -116,7 +119,7 @@ def monte_carlo_simulation(
     # Optimal period of exercise is the earliest time that exercise is triggered. If no exercise, an NA is returned:
     exercise_timings = np.full(shape=number_simulations, fill_value=np.nan)
 
-    ## Would we exercise at option termination?
+    # Would we exercise at option termination?
     exercise = profit[-1,] > 0
 
     # Receive immediate profit if exercising:
@@ -125,11 +128,11 @@ def monte_carlo_simulation(
     # Was the option exercised?
     exercise_timings[exercise] = termination_period
 
-    ## American Options hold value in waiting:
-    ## Backwards induction begin:
+    # American Options hold value in waiting:
+    # Backwards induction begin:
     for t in range(termination_period - 1, -1, -1):
 
-        ## Immediate payoff of exercise:
+        # Immediate payoff of exercise:
         profit_t = profit[t, :]
 
         # We only consider the exercise / delay exercise decision for price paths that are in the money (ie. profit from immediate exercise > 0):
@@ -153,7 +156,8 @@ def monte_carlo_simulation(
         exercise = profit_t > continuation_value
 
         # Discount existing values if not exercising
-        real_option_value[~exercise] = real_option_value[~exercise] * discount_rate
+        real_option_value[~exercise] = real_option_value[~exercise] * \
+            discount_rate
         # Receive immediate profit if exercising
         real_option_value[exercise] = profit_t[exercise]
 
@@ -163,18 +167,18 @@ def monte_carlo_simulation(
         # Re-iterate.
     # End backwards induction.
 
-    ## TODO: One more required inconsistency between real_options and american_options?
+    # TODO: One more required inconsistency between real_options and american_options?
     # real_option_value = real_option_value * discount_rate
 
-    ## Evaluate outputs:
+    # Evaluate outputs:
     return RealOption(
         profit=profit,
         real_option_value=real_option_value,
-        exercise_timings=exercise_timings, 
+        exercise_timings=exercise_timings,
         net_cash_flow=net_cash_flow,
         capital_expenditure=capital_expenditure,
         construction_periods=construction_periods,
-        number_simulations=number_simulations, 
+        number_simulations=number_simulations,
         number_periods=number_periods,
         time_step=time_step
-            )
+    )
